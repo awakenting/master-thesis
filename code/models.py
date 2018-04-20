01e5e7fde66d1e620049809a6d0d61b0ad52eef8
Andrej Warkentin <an.warkentin@web.de>
2018-04-20 13:50:09 +0200

    did some cleanup

--- code/models.py
+++ code/models.py
@@ -1,6 +1,11 @@
 import numpy as np
 from numba import jit
 
+
+def f_theta_linear(theta, m, b):
+    return theta*m + b
+
+
 def generate_stimulus(stim_size=1, speed=1, length=10, dt=0.1, init_distance=50, init_period=0, cutoff_angle=None):
     total_length = length + init_period
     stim_timesteps = np.arange(np.ceil(length/dt))*dt
@@ -16,24 +21,24 @@ def generate_stimulus(stim_size=1, speed=1, length=10, dt=0.1, init_distance=50,
         angles_degrees[cutoff_mask] = cutoff_angle
     return all_timesteps, angles_degrees, distances
 
-def f_theta_linear(theta, m, b):
-    return theta*m + b
 
 def transform_stim(stim_size, speed, length, dt, m=1.5, b=0, init_period=0, cutoff_angle=None, init_distance=50):
     t, stims, dists = generate_stimulus(stim_size=stim_size, speed=speed, length=length, dt=dt, init_period=init_period,
                                         cutoff_angle=cutoff_angle, init_distance=init_distance)
+    transformed_stims = f_theta_linear(stims, m=m, b=b)
+
     collision_idx = np.argmin(np.abs(dists))
     t_collision = t[collision_idx]
-    stim_collision = stims[collision_idx]
-    before_collision_mask = t<t_collision
+    before_collision_mask = t < t_collision
+
     t_to_collision = t[before_collision_mask] - t_collision
-    stim_to_collision = stims[before_collision_mask]
-    transformed_stim_to_collision = f_theta_linear(stim_to_collision, m=m, b=b)
-    transformed_stims = f_theta_linear(stims, m=m, b=b)
+    transformed_stim_to_collision = transformed_stims[before_collision_mask]
+
     return t, stims, transformed_stims, dists, t_to_collision, transformed_stim_to_collision
 
-def lif_dynamics(tau_m, e_l, r_m, stimulus, noise, v_t, dt, total_time, init_vm_std, vt_std):
-    ntime_steps = int(total_time/dt)
+
+def lif_dynamics(tau_m, e_l, r_m, stimulus, noise, v_t, dt, total_time, init_vm_std, vt_std, init_period):
+    ntime_steps = int((total_time + init_period)/dt)
     time_points = np.arange(ntime_steps)*dt
     v_m = np.zeros(ntime_steps)
     v_m[0] = np.random.normal(loc=e_l, scale=init_vm_std)
@@ -46,11 +51,13 @@ def lif_dynamics(tau_m, e_l, r_m, stimulus, noise, v_t, dt, total_time, init_vm_
         v_m[t] = v_m[t - 1] + dt*(- (v_m[t - 1] - e_l) + r_m*stimulus[t])/tau_m + noise[t]
         if v_m[t] > v_t[t]:
             v_m[t] = e_l
-            t_spks.append(t*dt)
-            idx_spks.append(t)
+            if t * dt > init_period:
+                t_spks.append(t*dt)
+                idx_spks.append(t)
         t = t + 1
 
     return time_points, v_m, t_spks, idx_spks
+
 
 @jit
 def jit_lif_dynamics(tau_m, e_l, r_m, stimulus, noise, v_t, dt, total_time, init_vm_std, vt_std, init_period):
@@ -90,10 +97,7 @@ def jit_ffi_model(tau_m, e_l, r_m, stimulus, noise_exc, noise_inh, v_t, dt, tota
     t = 1
     while t < ntime_steps:
         # calculate activation of inhibitory population
-        rho_inh[t] = rho_inh[t - 1] + dt * (rho_null - rho_inh[t - 1] + rho_scale * stimulus[t]) / tau_inh + noise_inh[
-            t]
-        if t % 0.01 == 0:
-            print(rho_inh[t])
+        rho_inh[t] = rho_inh[t - 1] + dt * (rho_null - rho_inh[t - 1] + rho_scale * stimulus[t]) / tau_inh + noise_inh[t]
         # calculate lif dynamics with inhibitory input
         v_m[t] = v_m[t - 1] + dt * (- (v_m[t - 1] - e_l) + r_m * stimulus[t] - rho_inh[t - 1]) / tau_m + noise_exc[t]
         if v_m[t] > v_t[t]:
@@ -105,23 +109,27 @@ def jit_ffi_model(tau_m, e_l, r_m, stimulus, noise_exc, noise_inh, v_t, dt, tota
 
     return time_points, v_m, t_spks, idx_spks, rho_inh
 
-def calc_response(params):
-    lv = np.random.rand()*1.1 + 0.1
-    stim_size = np.random.rand()*15 + 10
-    speed = 1/(lv/stim_size)
+
+def calc_response_lif(params):
+    # sample lv values uniformly between lv_min and lv_max
+    lv = np.random.rand() * (params['lv_max'] - params['lv_min']) + params['lv_min']
+    # sample stimulus sizes (L) uniformly between l_min and l_max
+    # stim_size = np.random.rand()*(params['l_max']-params['l_min']) + params['l_min']
+    stim_size = np.random.randint(params['l_min'], params['l_max'])
+    speed = 1 / (lv / stim_size)
     t, stims, tstims, dists, t_to_coll, tstim_to_coll = transform_stim(stim_size, speed, params['total_time'],
                                                                        params['dt'], params['m'], params['b'],
-                                                                       params['init_period'], params['cutoff_angle'])
+                                                                       params['init_period'], params['cutoff_angle'],
+                                                                       params['init_distance'])
 
     stimulus = tstims*1e-11
     sigma = params['noise_std'] * np.sqrt(params['dt'])
     noise_vals = np.random.normal(loc=0.0, scale=sigma, size=len(stimulus))
 
-    #np.random.seed(1)
     time, v_m, spks, spk_idc = jit_lif_dynamics(params['tau_m'], params['e_l'], params['r_m'], stimulus, noise_vals,
                                                 params['v_t'], params['dt'], params['total_time'], params['init_vm_std'],
                                                 params['vt_std'], params['init_period'])
-    if not len(spks)==0:
+    if not len(spks) == 0:
         first_spike = spks[0]
         first_spike_idx = spk_idc[0]
     else:
@@ -142,9 +150,9 @@ def calc_response_ffi(params):
     stim_size = np.random.randint(params['l_min'], params['l_max'])
     speed = 1 / (lv / stim_size)
     t, stims, tstims, dists, t_to_coll, tstim_to_coll = transform_stim(stim_size, speed, params['total_time'],
-                                                                          params['dt'], params['m'], params['b'],
-                                                                          params['init_period'], params['cutoff_angle'],
-                                                                          params['init_distance'])
+                                                                       params['dt'], params['m'], params['b'],
+                                                                       params['init_period'], params['cutoff_angle'],
+                                                                       params['init_distance'])
 
     stimulus = tstims * 1e-11 * params['exc_scale']
     sigma_exc = params['noise_std_exc'] * np.sqrt(params['dt'])
@@ -156,10 +164,10 @@ def calc_response_ffi(params):
     #rho_null = np.random.exponential(scale=params['rho_null'])
     rho_null = rho_null / 1000
     time, v_m, spks, spk_idc, rho_inh = jit_ffi_model(params['tau_m'], params['e_l'], params['r_m'], stimulus,
-                                                      noise_exc,
-                                                      noise_inh, params['v_t'], params['dt'], params['total_time'],
-                                                      params['init_vm_std'], params['vt_std'], rho_null,
-                                                      params['tau_inh'], params['rho_scale'], params['init_period'])
+                                                      noise_exc, noise_inh, params['v_t'], params['dt'],
+                                                      params['total_time'], params['init_vm_std'], params['vt_std'],
+                                                      rho_null, params['tau_inh'], params['rho_scale'],
+                                                      params['init_period'])
     if not len(spks) == 0:
         first_spike = spks[0]
         first_spike_idx = spk_idc[0]
@@ -176,15 +184,15 @@ def calc_response_ffi(params):
 
 
 def lv_map(lv):
-    if lv > 0.1 and lv < 0.28:
+    if 0.1 < lv < 0.28:
         return 0.19
-    elif lv > 0.28 and lv < 0.47:
+    elif 0.28 < lv < 0.47:
         return 0.38
-    elif lv > 0.47 and lv < 0.65:
+    elif 0.47 < lv < 0.65:
         return 0.56
-    elif lv > 0.65 and lv < 0.83:
+    elif 0.65 < lv < 0.83:
         return 0.74
-    elif lv > 0.83 and lv < 1.01:
+    elif 0.83 < lv < 1.01:
         return 0.92
     else:
         return 1.11
